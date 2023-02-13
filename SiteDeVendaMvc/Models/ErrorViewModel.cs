83c5bb2e00676eb06dc7a55b7c6d240329585810
Jucilo Benguela <jucilobenguela@hotmail.com>
2023-02-13 21:15:00 +0000

    Creação do Projecto

---
+++ SiteDeVendaMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SiteDeVendaMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
